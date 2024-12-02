49609d30a6081508d15cb9f803630b68887ef243
Miles Nelson <miles.nelson@cityplumbing.co.uk>
2024-12-02 22:12:27 +0000

    removed dumb type conversion, over complicated

--- 2024/1/one.go
+++ 2024/1/one.go
@@ -2,7 +2,6 @@ package one
 
 import (
 	"bufio"
-	"math"
 	"os"
 	"slices"
 	"strconv"
@@ -13,14 +12,15 @@ func OneMain() int {
 
 	left, right := intiilisation()
 
-	// square and root to remove negative
+	// negative to positive
 	// tally up difference
-	var total float64
+	var total int
 	for i := 0; i < len(left); i++ {
-		value1 := left[i] - right[i]
-		var value2 float64 = float64(value1 * value1)
-		value2 = math.Sqrt(value2)
-		total += value2
+		value := left[i] - right[i]
+		if value < 0 {
+			value = -value
+		}
+		total += value
 	}
 
 	return int(total)
@@ -32,7 +32,7 @@ func OneMain() int {
 func OneSec() int {
 	left, right := intiilisation()
 
-	dic := make(map[float64]float64)
+	dic := make(map[int]int)
 	// maybe some optimisations because its sorted? cba
 	for i := 0; i < len(right); i++ {
 		_, exists := dic[right[i]]
@@ -43,7 +43,7 @@ func OneSec() int {
 		}
 	}
 
-	var total float64
+	var total int
 	for i := 0; i < len(left); i++ {
 		_, exists := dic[left[i]]
 		if exists {
@@ -51,24 +51,23 @@ func OneSec() int {
 		}
 	}
 
-	return int(total)
+	return total
 }
 
 // both functions need the file into an array first needs sorted
-func intiilisation() ([]float64, []float64) {
+func intiilisation() ([]int, []int) {
 
 	file, _ := os.Open("2024/data/dayOne.txt")
 	defer file.Close()
 
-	var left []float64
-	var right []float64
+	var left []int
+	var right []int
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		nums := strings.Split(scanner.Text(), "   ")
-		// convert to float 64 because squaring and rooting to remove negatives
-		value1, _ := strconv.ParseFloat(nums[0], 64)
-		value2, _ := strconv.ParseFloat(nums[1], 64)
+		value1, _ := strconv.Atoi(nums[0])
+		value2, _ := strconv.Atoi(nums[1])
 		left = append(left, value1)
 		right = append(right, value2)
 	}
