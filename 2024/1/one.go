a8185b1e97f5cfcb3217f440286086999e704a63
Miles Nelson <miles.nelson@cityplumbing.co.uk>
2024-12-02 21:25:13 +0000

    day one comments

--- 2024/1/one.go
+++ 2024/1/one.go
@@ -13,6 +13,9 @@ import (
 func OneMain() int {
 
 	left, right := intiilisation()
+
+	// square and root to remove negative
+	// tally up difference
 	var total float64 = 0
 	for i := 0; i < len(left); i++ {
 		value1 := left[i] - right[i]
@@ -24,16 +27,15 @@ func OneMain() int {
 	return int(total)
 }
 
+// gets right list count
+// multiply left value with right count
+// return total product of the latter
 func OneSec() int {
 	left, right := intiilisation()
 
-	type value struct {
-		value   int
-		numOcur int
-	}
 	var total int = 0
 	dic := make(map[int]int)
-
+	// maybe some optimisations because its sorted? cba
 	for i := 0; i < len(right); i++ {
 		_, exists := dic[int(right[i])]
 		if exists {
@@ -53,6 +55,7 @@ func OneSec() int {
 	return total
 }
 
+// both functions need the file into an array first needs sorted
 func intiilisation() ([]float64, []float64) {
 	file, err := os.Open("2024/data/dayOne.txt")
 	if err != nil {
@@ -66,6 +69,7 @@ func intiilisation() ([]float64, []float64) {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		nums := strings.Split(scanner.Text(), "   ")
+		// convert to float 64 because squaring and rooting to remove negatives
 		value1, err1 := strconv.ParseFloat(nums[0], 64)
 		value2, err2 := strconv.ParseFloat(nums[1], 64)
 		if err1 != nil || err2 != nil {
@@ -75,6 +79,7 @@ func intiilisation() ([]float64, []float64) {
 		right = append(right, value2)
 	}
 
+	//sort :)
 	slices.Sort(left)
 	slices.Sort(right)
 
